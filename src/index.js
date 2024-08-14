f257851c4da6ad8b431608beeb3d7fce519abbc1
Ahmad Mustafa <134202213+Ahmad897m@users.noreply.github.com>
2024-08-14 12:24:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import './index.css';
 import App from './App';
 import {
   BrowserRouter,
+  HashRouter,
   Routes,
   Route
 } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
   <Routes>
     <Route path = '/' element = {<App />}></Route>
   </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
